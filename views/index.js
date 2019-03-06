48337dc1efdc9da509b371f55a34ebc619d68299
Rebecca Hom <rebecca.hom001@gmail.com>
2019-03-05 18:53:12 -0800

    reverted API routes

--- views/index.js
+++ views/index.js
@@ -1,21 +1,9 @@
 const path = require("path");
 const router = require("express").Router();
-const booksController = require("../controllers/booksController");
-// const apiRoutes = require("./api");
+const apiRoutes = require("./api");
 
 // API Routes
-// router.use("/api", apiRoutes);
-// Matches with "/api/books"
-router.route("/api/books")
-  .get(booksController.findAll)
-  .post(booksController.create);
-
-// Matches with "/api/books/:id"
-router
-  .route("/api/books/:id")
-  .get(booksController.findById)
-  .put(booksController.update)
-  .delete(booksController.remove);
+router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
