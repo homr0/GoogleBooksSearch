90b2459dd79c55d1c365a1f57bed80fff1670c08
Rebecca Hom <rebecca.hom001@gmail.com>
2019-03-05 19:46:30 -0800

    updated Navbar font color to match header image

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,17 +6,17 @@ const Navbar = () => {
   return(
     <nav>
       <div className="nav-wrapper deep-purple lighten-3">
-        <a className="brand-logo left" href="/">Boogle</a>
+        <a className="brand-logo left deep-purple-text text-darken-4" href="/">Boogle</a>
 
         <ul id="nav-mobile">
           <li>
-            <Link to="/search" className={window.location.pathname === "/search" ? "active" : ""}>
+            <Link to="/search" className={"deep-purple-text text-darken-4 "+ (window.location.pathname === "/search" ? "active" : "")}>
               Search
             </Link>
           </li>
 
           <li>
-            <Link to="/saved" className={window.location.pathname === "/saved" ? "active" : ""}>
+            <Link to="/saved" className={"deep-purple-text text-darken-4 " + (window.location.pathname === "/saved" ? "active" : "")}>
               Saved
             </Link>
           </li>
