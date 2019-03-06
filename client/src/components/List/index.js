f6f2acd6192b9df2a94df356a755e7a5c72e8bd6
Rebecca Hom <rebecca.hom001@gmail.com>
2019-03-05 19:53:22 -0800

    updated body background and List colors

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 export const List = ({children, header}) => {
   return(
-    <ul className="collection">
+    <ul className="collection deep-purple lighten-4 deep-purple-text text-darken-4">
       <li className="collection-header"><h4>{header}</h4></li>
       {children}
     </ul>
@@ -12,7 +12,7 @@ export const List = ({children, header}) => {
 
 export const ListItem = ({children}) => {
   return(
-    <li className="collection-item">
+    <li className="collection-item indigo-text text-darken-4">
       {children}
     </li>
   );
